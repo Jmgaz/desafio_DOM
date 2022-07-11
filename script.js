503c506a815baf9ab3e8ae7c74b1c331331ec6f7
Juan Martín Carsillo <juanmartincarsillo@gmail.com>
2022-07-11 20:46:36 -0300

    Desafío eventos

--- script.js
+++ script.js
@@ -63,11 +63,51 @@ for(const combo of listaCombos){
         <div class="card" style="width: 18rem;">
             <div class="card-body">
                 <h5 class="card-title">${combo.nombre}</h5>
-                <p class="card-text"> Ingredientes: ${combo.rolls}</p>
-                <p class="card-text">Precio: <b>$${combo.precio}</b> </p>
-                <p class="card-text">También tenemos alternativas vegetarianas y veganas</p>
+                <button class="btn btn-primary" id="btnRolls${combo.id}">Ingredientes</button>
+                <button class="btn btn-primary" id="btnPrecio${combo.id}">Precio</button>
             </div>
         </div>`
 
 contenedorCombos.append(columna)
 }
+
+let botonRolls1 = document.getElementById("btnRolls1")
+botonRolls1.addEventListener("click",() =>{
+    alert(listaCombos[0].rolls)
+})
+let botonRolls2 = document.getElementById("btnRolls2")
+botonRolls2.addEventListener("click",() =>{
+    alert(listaCombos[1].rolls)
+})
+let botonRolls3 = document.getElementById("btnRolls3")
+botonRolls3.addEventListener("click",() =>{
+    alert(listaCombos[2].rolls)
+})
+let botonRolls4 = document.getElementById("btnRolls4")
+botonRolls4.addEventListener("click",() =>{
+    alert(listaCombos[3].rolls)
+})
+let botonRolls5 = document.getElementById("btnRolls5")
+botonRolls5.addEventListener("click",() =>{
+    alert(listaCombos[4].rolls)
+})
+let botonPrecio1 = document.getElementById("btnPrecio1")
+botonPrecio1.addEventListener("click",() =>{
+    alert(listaCombos[0].precio)
+})
+let botonPrecio2 = document.getElementById("btnPrecio2")
+botonPrecio2.addEventListener("click",() =>{
+    alert(listaCombos[1].precio)
+})
+let botonPrecio3 = document.getElementById("btnPrecio3")
+botonPrecio3.addEventListener("click",() =>{
+    alert(listaCombos[2].precio)
+})
+let botonPrecio4 = document.getElementById("btnPrecio4")
+botonPrecio4.addEventListener("click",() =>{
+    alert(listaCombos[3].precio)
+})
+let botonPrecio5 = document.getElementById("btnPrecio5")
+botonPrecio5.addEventListener("click",() =>{
+    alert(listaCombos[4].precio)
+})
